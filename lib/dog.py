37959b46d22318716aeffb14e09d63f752d9326a
Caleb <kamenjuhkiiru@gmail.com>
2023-11-27 18:11:41 +0300

    Work in progress

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,42 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = None, breed = None):
+        self._name = None
+        self._breed = None
+
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        print("Retrieving Name")
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str) and 1 < len(name) < 25:
+            self._name = name
+            print(name)
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_breed(self):
+        print("Retrieving Breed")
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+            print(breed)
+        else:
+            print("Breed must be in the list of approved breeds.")
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+# Test case
+fido = Dog()
+fido.name = "Alicia Keys"  # Valid name
+fido.breed = "Pug"   # Valid breed
+
+fido.name = 123      # Invalid name, prints: "Name must be a string between 1 and 25 characters."
+fido.breed = "Labrador"  # Invalid breed, prints: "Breed must be in the list of approved breeds."
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,48 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self._name = None
+        self._job = None
+
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        print(self._name)
+        return self._name
+    
+    def set_name(self, name):
+        if (type(name) == str) and (1 < len(name) < 25):
+            name_title_case = name.title()
+            self._name = name_title_case
+            print(name_title_case)
+        else:
+            error_message = "Name must be string between 1 and 25 characters."
+            print(error_message)
+            return error_message
+
+    def get_job(self):
+        print(self._job)
+        return self._job
+    
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+            print(job)
+        else:
+            error_message ="Job must be in list of approved jobs."
+            print(error_message)
+            return error_message
+    
+    name = property(get_name, set_name,)
+    job = property(get_job, set_job)
+
+
+# test cases
+x = Person()
+x.name = 'ALI' # Invalid name, prints: "Name must be a string under 25 characters."
+x.job = "" # Invalid name, prints:  "Job must be in list of approved jobs."
+
+x.name = 'Guido'  # Valid name
+x.job = 'Sales'  # Valid job
